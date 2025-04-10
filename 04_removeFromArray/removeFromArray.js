728951da68a3b7de22c83adb5e990ef4dab1b483
Aaron Hammer-Huber <202416678+AaronHamHub@users.noreply.github.com>
2025-04-10 14:29:12 -0400

    Excercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let output = [];
+    for(let i = 0; i < array.length; i++)
+    {
+        let shouldAddElement = true;
+        for(let j = 0; j < args.length; j++)
+        {
+            if (array[i] === args[j])
+                {
+                shouldAddElement = false;
+                break;
+            }
+        }
+        if(shouldAddElement)
+        {
+            output.push(array[i]);
+        }
+    }
+
+    console.log(output.toString());
+
+    return output;
 };
 
 // Do not edit below this line
