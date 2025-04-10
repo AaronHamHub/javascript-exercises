668eb738b7c1639f61556e4ff65cc59b855ca789
Aaron Hammer-Huber <202416678+AaronHamHub@users.noreply.github.com>
2025-04-10 14:00:07 -0400

    Exercise 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(toRepeat, numRepeats) {
+    if(numRepeats < 0)
+    {
+        return "ERROR";
+    }
+    
+    let output = "";
 
+    for(let i = 0; i < numRepeats; i++)
+    {
+        output += toRepeat;
+    }
+
+    return output;
 };
 
 // Do not edit below this line
