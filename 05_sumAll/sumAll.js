b8ccdbd760201234bd690a8e452d7095fdf97432
Aaron Hammer-Huber <202416678+AaronHamHub@users.noreply.github.com>
2025-04-10 14:39:33 -0400

    Exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if(num1 < 0 || num2 < 0) return "ERROR";
+
+    // Assume num1 is bigger, if we are wrong then swap them
+    let bigger = num1;
+    let smaller = num2;
+    if (num1 < num2)
+    {
+        bigger = num2;
+        smaller = num1;
+    }
+
+    let sum = 0;
+    for (let i = smaller; i <= bigger; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
