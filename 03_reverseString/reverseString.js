c5b0f5213bb2e2ce62696b442e04835854c2557f
Aaron Hammer-Huber <202416678+AaronHamHub@users.noreply.github.com>
2025-04-10 14:08:00 -0400

    Exercise 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(toReverse) {
+    let output = toReverse.split("");
 
+    for(let i = 0; i < output.length / 2; i++)
+    {
+        let temp = output[i];
+        output[i] = output [output.length - i - 1];
+        output[output.length - i - 1] = temp;
+    }
+
+    return output.join("");
 };
 
 // Do not edit below this line
